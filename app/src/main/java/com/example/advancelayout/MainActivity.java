44b19a20c0023ef26f35d6802d52ad5b42c24407
islombeknasriddinov <nasriddinov.islom.19@gmail.com>
2022-06-21 17:44:21 +0500

    Initial commit

---
+++ app/src/main/java/com/example/advancelayout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.advancelayout;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        EditText et_key = findViewById(R.id.et_key);
+        et_key.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i== EditorInfo.IME_ACTION_DONE){
+                    Log.d("Tag","Key board done pressed");
+                }
+                return false;
+            }
+
+        });
+    }
+}
